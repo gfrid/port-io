4ac39c41ccc9723ecf7f4be659c99989757c17a2
George <97164735+gfrid@users.noreply.github.com>
2023-05-11 11:45:38 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -11,4 +11,21 @@ provider "port-labs" {
 	client_id = "wHjVxzUMNU2lSEIusD1HxAuNnYkHWKFt"     # or set the env var PORT_CLIENT_ID
 	secret    = "4k39UAR2rojvBUDG4zcqp9rQMn1nC1iyJBOwFXGZHJTpCpbpPfOJg89ZK5T0BZFb" # or set the env var PORT_CLIENT_SECRET
 	base_url  = "https://api.getport.io/v1"
+
+}
+
+resource "port-labs_entity" "1212" {
+    title      = "test"
+    identifier = "1212"
+    blueprint  = "package"
+
+    properties {
+		name  = "language"
+		value = "Python"
+	}
+
+	properties {
+		name  = "version"
+		value = "string"
+	}
 }
